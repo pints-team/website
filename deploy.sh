35924ce3ff296e9d500e7d28ee9074957a794535
Fergus Cooper <fcooper8472@gmail.com>
2019-05-16 16:25:38 +0100

    Update deploy to not delete from untracked

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ HOST=linux2.cs.ox.ac.uk
 DIR=/fs/website/projects/PINTS/
 
 hugo 
-rsync -avz --no-perms --delete public/ ${USER}@${HOST}:${DIR}
+rsync -avz --exclude 'untracked/*' --no-perms --delete public/ ${USER}@${HOST}:${DIR}
 
 ssh ${USER}@${HOST} "chmod -R g+w ${DIR}"
 
