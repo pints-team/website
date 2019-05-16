d71057a817f116d8f5e8cbb4d8d9e70c9bc1ed9b
Fergus Cooper <fcooper8472@gmail.com>
2019-05-16 15:44:12 +0100

    Update deploy to add group write permissions to new files

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,8 @@ HOST=linux2.cs.ox.ac.uk
 DIR=/fs/website/projects/PINTS/
 
 hugo 
-chmod -R g+w public/*
-rsync -avz --no-owner --no-perms --delete public/ ${USER}@${HOST}:${DIR}
+rsync -avz --no-perms --delete public/ ${USER}@${HOST}:${DIR}
+
+ssh ${USER}@${HOST} "chmod -R g+w ${DIR}"
 
 exit 0
