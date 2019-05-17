d994b88b0597c9c2a8ffa31d52452aacd46a291b
Fergus Cooper <fcooper8472@gmail.com>
2019-05-17 12:18:14 +0100

    Change untracked dir to functional-testing

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ HOST=linux2.cs.ox.ac.uk
 DIR=/fs/website/projects/PINTS/
 
 hugo 
-rsync -avz --exclude='untracked/*' --exclude='functional-testing/index.html' --no-perms --delete public/ ${USER}@${HOST}:${DIR}
+rsync -avz --exclude='functional-testing/*' --exclude='functional-testing/index.html' --no-perms --delete public/ ${USER}@${HOST}:${DIR}
 
 ssh ${USER}@${HOST} "chmod -R g+w ${DIR}"
 
